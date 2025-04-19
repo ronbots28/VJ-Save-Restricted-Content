0e4e7b02a14cfaa94e5dd49fce8554b1a2d11378
ronbots28 <chaturvedirahul1997@gmail.com>
2025-04-19 13:52:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7020025780:AAFjT5CFQufnkjbcFEyD6vzSUwvKFQf6ewc")
 
 # Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", "26086499"))
@@ -18,4 +18,4 @@ DB_URI = os.environ.get("DB_URI", "mongodb+srv://fogizopi:nruxgyxho9waCovT@clust
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
-ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
+ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', false))
